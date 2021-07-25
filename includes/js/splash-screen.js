f1934b261d6995b51839db1561cc5ca8fc93f47b
nghi-nguyen-beep <nghin@mit.edu>
2021-07-24 20:59:42 -0500

    Remove splash screen

--- includes/js/splash-screen.js
+++ includes/js/splash-screen.js
@@ -1,25 +1,40 @@
 // $(".splash").delay(2000).fadeIn("slow");
-window.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
 
-        document.querySelectorAll('.logo').forEach((span, idx) => {
-            setTimeout(() => {
-                span.classList.add("active");
-            }, (idx + 1) * 500)
-        });
-
-        setTimeout(() => {
-            document.querySelectorAll('.logo').forEach((span, idx) => {
-                setTimeout(() => {
-                    span.classList.remove("active");
-                    span.classList.add("fade");
-                }, (idx + 1) * 100)
-            })
-        }, 2000);
+function enterHome() { 
 
         setTimeout(() => {
             document.querySelector('.intro').style.top = "-100vh";
-        }, 3000)
+            document.querySelector('.main-Home').fadeIn();
+            }, 500)
+    
+};
 
-    })
+window.addEventListener('DOMContentLoaded', () => {
+
+ 
+        setTimeout(() => {
+            document.querySelectorAll('.logo').forEach((span, idx) => {
+                setTimeout(() => {
+                    span.classList.add("active");
+                }, (idx + 1) * 700)
+            });
+        });
+    
+   
+
+
+        // setTimeout(() => {
+        //     document.querySelectorAll('.logo').forEach((span, idx) => {
+        //         setTimeout(() => {
+        //             span.classList.remove("active");
+        //             span.classList.add("fade");
+        //         }, (idx + 1) * 100)
+        //     })
+        // }, 2000);
+
+        // setTimeout(() => {
+        //     document.querySelector('.intro').style.top = "-100vh";
+        // }, 3000)
+
+
 })
